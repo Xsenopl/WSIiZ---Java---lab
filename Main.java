dc5ccb28233aaa3ab0eb49537248deae4356db32
Xsenopl <49074078+Xsenopl@users.noreply.github.com>
2022-10-16 18:09:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.util.function.IntToDoubleFunction;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
         String imie = "Jan", miasto = "Rzeszów";
         int wiek = 24;
         //      System.out.println("Imie: "+imie+ ", miasto: "+miasto+ ", wiek: "+wiek);
 
-        //Wczytywanie danych z klawiatury
+
+
+                                                    //______Wczytywanie danych z klawiatury
         Scanner input = new Scanner(System.in);
         System.out.printf("Podaj wiek: ");
         wiek = input.nextInt();
 
+/*
         System.out.printf("Podaj imię: ");
         imie = input.next();
         System.out.println("Next " + imie);
@@ -21,15 +26,30 @@ public class Main {
 
         imie = input.nextLine();
         System.out.println("NextLine " + imie);
+*/
+
 
         System.out.println(getMyName());
         System.out.println(getMyAge());
-        obliczenia(1,3);
-        wiek = input.nextInt();
-      //  zad3(wiek);
+        System.out.println("Podaj dwie liczby: ");
+        int a, b;
+        a = input.nextInt();    b = input.nextInt();
+        obliczenia(a,b);
+        System.out.println("Zad 3: "+zad3(a));
+        System.out.println("Zad 4: "+zad4(a));
+        System.out.println("Zad 5: "+zad5((double)a));
+        System.out.println("Zad 6: "+zad6((double)a));
+        System.out.println("Podaj dwie liczby, z których przedziału będzie program losował do zadania 7: ");
+        a = input.nextInt();    b = input.nextInt();
+        Random generator = new Random();
+        int x = generator.nextInt(b-a)+a;
+        int y = generator.nextInt(b-a)+a;
+        int z = generator.nextInt(b-a)+a;
+        System.out.println("Zad 7: "+zad7(x,y,z));
     }
 
-//________________________________________________________________________________
+
+    //________________________________________________________________________________
 //  ___Zadanie 1___
     public static String getMyName(){
         return "Bogdan";
@@ -38,21 +58,37 @@ public class Main {
         return 21;
     }
 
-//  ___Zadanie 2___
+    //  ___Zadanie 2___
     public static void obliczenia(int l1, int l2){
         System.out.println( "Suma "+ l1 +" i "+l2+ " = "+(l1+l2));
         System.out.println( "Różnica "+ l1 +" i "+l2+ " = "+(l1-l2));
         System.out.println( "Iloczyn "+ l1 +" i "+l2+ " = "+(l1*l2));
     }
-//  ___Zadanie 3___
-    public static boolean zad3(){
-        int a,b;
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Podaj a i b: ");
-        a = input.nextInt();
-        b = input.nextInt();
-    return true;
+    //  ___Zadanie 3___
+    public static boolean zad3(int a){
+        if(a%2==0){
+            return true;
+        }
+        else return false;
     }
-
-
+    public static boolean zad4(int a) {
+        if(a%3==0 && a%5==0){
+            return true;
+        }
+        else return false;
+    }
+    public static double zad5(double a){
+        a=Math.pow(a,3);
+        return a;
+    }
+    public static double zad6(double a){
+        a=Math.sqrt(a);
+        return a;
+    }
+    //___Zadanie 7___
+    public static boolean zad7(int x,int y,int z){
+        if(x*x==y*y+z*z && x*x+y*y==z*z && x*x+z*z==y*y)
+            return true;
+        else return false;
+    }
 }
