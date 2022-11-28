760c8a41802bb7fdc3719ac998f779fa408c9545
Xsenopl <49074078+Xsenopl@users.noreply.github.com>
2022-11-28 19:57:41 +0100

    Update Klient.java

--- src_Lab5/Lab5b/Klient.java
+++ src_Lab5/Lab5b/Klient.java
@@ -1,8 +1,13 @@
 package Lab5b;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klient {
     String imie, nazwisko;
     int id;
+    List<Ksiazka> posiadane = new ArrayList<Ksiazka>();
+
 
     public Klient(String imie, String nazwisko) {
         this.imie = imie;
@@ -12,5 +17,20 @@ public class Klient {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.id = id;
+    }
+    public void DodajKsiazke(Ksiazka k){
+        posiadane.add(k);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void Przegladaj() {
+        int i = 0;
+        for (Ksiazka ksiazka : posiadane) {
+            System.out.printf("\n" + i + ". ");
+            ksiazka.opis();
+            i++;
+        }
     }
 }
